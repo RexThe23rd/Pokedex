f026e3b2bf99923254a5b1652855047f4717034c
RexThe23rd <rexthe23rd@gmail.com>
2025-08-08 12:22:41 -0500

    basic sidenav (its ugly)

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Pokedex following Smoljames's Reactjs course
+Pokedex following Smoljames' Reactjs course
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,5 +1,16 @@
+import {first151Pokemon, getFullPokedexNumber} from "../utils"
+
 export function SideNav() {
     return(
-        <nav></nav>
+        <nav>
+            {first151Pokemon.map((pokemon, pokemonIndex) => {
+                return(
+                    <button>
+                        <p>{getFullPokedexNumber(pokemonIndex)}</p>
+                        <p>{pokemon}</p>
+                    </button>
+                )
+            })}
+        </nav>
     )
 }
