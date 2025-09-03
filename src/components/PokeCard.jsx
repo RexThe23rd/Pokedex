3e9e2f0936e167bf50a30bebfc071a3b8821cbab
RexThe23rd <rexthe23rd@gmail.com>
2025-09-03 13:32:24 -0500

    added some logic to the pokecard and fixed issues

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,4 +1,48 @@
-export function PokeCard() {
+import { useEffect, useState } from "react"
+import { getPokedexNumber } from "../utils"
+
+export function PokeCard(props) {
+    const { selectedPokemon } = props
+    const [data, setData] = useState("null")
+    const [loading, setLoading] = useState("false")
+
+        useEffect(() => {
+
+        if (loading || !localStorage) { return }
+        let cache = {}
+        if (localStorage.getItem('pokedex')) {
+            cache = JSON.parse(localStorage.getItem('pokedex'))
+        }
+
+        if (selectedPokemon in cache) {
+            setData(cache[selectedPokemon])
+            console.log('Found pokemon in cache')
+            return
+        }
+
+        async function fetchPokemonData() {
+            setLoading(true)
+            try {
+                const baseUrl = 'https://pokeapi.co/api/v2/'
+                const suffix = 'pokemon/' + getPokedexNumber(selectedPokemon)
+                const finalUrl = baseUrl + suffix
+                const res = await fetch(finalUrl)
+                const pokemonData = await res.json()
+                setData(pokemonData)
+                console.log(pokemonData)
+                cache[selectedPokemon] = pokemonData
+                localStorage.setItem('pokedex', JSON.stringify(cache))
+            } catch (err) {
+                console.log(err.message)
+            } finally {
+                setLoading(false)
+            }
+        }
+
+        fetchPokemonData()
+
+    }, [selectedPokemon])
+
     return(
         <div></div>
     )
